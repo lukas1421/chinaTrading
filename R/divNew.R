4d24cb7be8d29b2c9771032ff88ed00027ff5d77
lukas1421 <shijiluke@gmail.com>
2017-08-04 13:57:50 +0800

    adding imports

--- R/divNew.R
+++ R/divNew.R
@@ -5,42 +5,44 @@ require(stringr)
 require(lubridate)
 require(plyr)
 
-
+#'get all divs
+#'
+#'
 getDivs <- function() {
   url <- getDivURLNew()
   a<-read_html(url)
   l<-html_nodes(a,"table")
   divText <- data.table(html_table(l[[3]]))
-  
+
   if(ncol(divText)!=3) {
     divText <- data.table(html_table(l[[4]]))
   }
-  
+
   names(divText) <- c("ticker","chineseName","divs")
-  
+
   divText[, ticker:=str_pad(ticker,width = 6,side = "left",pad = "0") ]
   divText[, ticker:=ifelse(str_sub(ticker,1,1)=="6", paste0("sh",ticker),paste0("sz",ticker) )]
-  
+
   #divText<- fread(paste0(tradingFolder,"divRaw1.txt"),header = TRUE)
   #divText <- data.table(read.table(paste0(tradingFolder,"divRaw1.txt"),header = TRUE,stringsAsFactors = F),keep.rownames = FALSE)
   #divText<- fread(paste0(tradingFolder,"divCSV.csv"),header = TRUE)
   #names(divText) <- c("ticker","chineseName","divs")
   #divText[, ticker:=str_pad(ticker,width = 6,side = "left",pad = "0") ]
-  
-  
+
+
   res<-divText[, c(chineseName,extractDiv1(ticker,divs)), by=.(ticker)]
-  
+
   res[, adjFactor:= ((lastPrice - cashDiv/10)/(1+as.numeric(stockDiv)/10))/lastPrice]
-  
+
   res[, adjPrice := ((lastPrice - cashDiv/10)/(1+as.numeric(stockDiv)/10))]
-  
+
   res<-res[!(!is.na(stockDivDate) & cashDivDate!=Sys.Date())]
   res<-res[!is.infinite(adjFactor) & !is.nan(adjFactor)]
-  
+
   res[, inList:=isInStockList(ticker), .(ticker) ]
-  
+
   res<-res[inList==TRUE, ]
-  
+
   write.table(res, paste0(tradingFolder,"div.txt"),quote = FALSE,sep = "\t")
   print(res)
   invisible()
@@ -65,7 +67,7 @@ extractDiv1 <- function(tickerFull,x) {
 getLastCloseV3 <- function(symb,dat) {
   ticker <- paste0(toupper(str_sub(symb,1,2)),"#",str_sub(symb,3))
   stock <- data.table()
-  tryCatch( 
+  tryCatch(
     {
       stock <- fread(paste0(dataFolder,ticker, ".txt"),header = TRUE,skip = 1,fill = T,
                      showProgress = TRUE,col.names = c("D","O","H","L","C","V","A"))
@@ -84,7 +86,7 @@ getDivURLNew <-function() {
   a<-read_html(url,encoding = "gbk")
   l<-html_nodes(a,"a")
   l1<-xml2::xml_attrs(l)
-  return((l1[which(lapply(l1,function(x) grep("沪深股市交易提示",x))>0)[1]])[[1]] %>% 
-           (function(x) x[which(names(x)=="href")])) 
+  return((l1[which(lapply(l1,function(x) grep("沪深股市交易提示",x))>0)[1]])[[1]] %>%
+           (function(x) x[which(names(x)=="href")]))
 }
 
