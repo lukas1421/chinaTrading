9cc64916d0991bfee5663883d63b3a0e987868be
lukas1421 <shijiluke@gmail.com>
2017-08-22 22:38:35 +0800

    822

--- inst/rcppTest.R
+++ inst/rcppTest.R
@@ -14,3 +14,37 @@ f<-function() {
   d[,get(V2,env) ,keyby=.(V2)]
 
 }
+
+f <- function() {
+  my_env <- environment()
+   call_env <- parent.frame()
+  # # parent.env(my_env) <- call_env
+  # # print("call env ")
+  # # print(call_env)
+  # print(ls())
+  # print("BEF")
+  # print(parent.env(my_env))
+  #parent.env(my_env) <- call_env
+  # print("AFTER")
+  # print(parent.env(my_env))
+
+
+
+   print(parent.env(my_env))
+   print(parent.frame())
+
+   print(parent.frame(2))
+
+  print(get(deparse(quote(y)),envir = .GlobalEnv))
+  print(get("y", envir = parent.env(my_env)))
+  print(get("y", envir = call_env))
+  print(get("y", envir = parent.frame(1)))
+  print(get("y", envir = parent.frame(2)))
+  invisible()
+}
+
+y <- 3
+g <- function() {
+  y <- 2
+  f()
+}
