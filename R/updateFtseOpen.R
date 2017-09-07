e539189429e18e01197b4e71b3d2fc6d1847b3b6
lukas1421 <shijiluke@gmail.com>
2017-09-08 06:46:25 +0800

    98 am

--- R/updateFtseOpen.R
+++ R/updateFtseOpen.R
@@ -9,7 +9,7 @@ updateTradeDateFTSEOpen <- function() {
   open <- readWorksheet(wb, sheet="2823",startRow = 250, endRow = 1000 , startCol = 10, endCol = 10)
   names(d) <-c("D")
   names(open) <- c("open")
-  m <- data.table(d,open)
+  m <- data.table(D=as.Date(d[1:nrow(open),1]),open)
   m <- m[D<Sys.Date(),][(.N-2):.N, ]
   write.table(m,paste0(getTradingFolder(),"ftseA50Open.txt"),quote = FALSE,sep = "\t",row.names = FALSE, col.names = FALSE)
   m
