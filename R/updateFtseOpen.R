e1df454a85dfbfdbd0e728ed10439e5acd354f26
lukas1421 <shijiluke@gmail.com>
2017-12-25 11:27:43 +0800

    1225 am

--- R/updateFtseOpen.R
+++ R/updateFtseOpen.R
@@ -7,10 +7,12 @@ updateTradeDateFTSEOpen <- function() {
   wb <- XLConnect::loadWorkbook(paste0(getTradingFolder(),"Premium and Discount_with macro.xlsm"))
   d <- XLConnect::readWorksheet(wb, sheet="2823",startRow = 400, endRow = 1000 , startCol = 1, endCol = 1,header=FALSE)
   open <- readWorksheet(wb, sheet="2823",startRow = 400, endRow = 1000 , startCol = 10, endCol = 10,header = FALSE)
+  cloze <- readWorksheet(wb, sheet="2823",startRow = 400, endRow = 1000 , startCol = 11, endCol = 11,header = FALSE)
   names(d) <-c("D")
   names(open) <- c("open")
+  names(cloze) <- c("close");
   e <- lubridate::ymd(d[1:nrow(open),1])
-  m <- data.table(D=e,open)
+  m <- data.table(D=e,open,cloze)
   m <- m[D<Sys.Date(),][(.N-2):.N, ]
   write.table(m,paste0(getTradingFolder(),"ftseA50Open.txt"),quote = FALSE,sep = "\t",row.names = FALSE, col.names = FALSE)
   m
